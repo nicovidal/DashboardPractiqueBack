89f2637d556c98c944f27dfedcebda332835085b
nicovidal <nico.vidal.m@gmail.com>
2023-12-21 20:18:32 -0300

    nadbar dark light mode ok

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { useState } from "react";
+import { Box, useMediaQuery } from "@mui/material";
+import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { NavBar } from "../../components/NavBar";
+
+
 
 export const Layout = () => {
   return (
-    <div>Layout</div>
-  )
-}
+    <Box width="100%" height="100%">
+      <Box>
+        <NavBar/>
+        <Outlet />
+      </Box>
+    </Box>
+  );
+};
