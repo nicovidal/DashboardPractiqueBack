f9a318534aa0289f357302209b6a8499149d162a
nicovidal <nico.vidal.m@gmail.com>
2023-12-22 19:04:39 -0300

    data products

--- client/src/scenes/products/index.jsx
+++ client/src/scenes/products/index.jsx
@@ -16,9 +16,20 @@ import { useGetProductsQuery } from "../../state/api.js";
 import { Header } from "../../components/Header";
 
 export const Products = () => {
+  const {data,isLoading}=useGetProductsQuery();
+  const isNonMobile=useMediaQuery("(min-width:1000px)")
+  console.log("data",data)
+
+
+
   return (
-    <Box>
+    <Box m="1.5rem 2.5rem">
       <Header title="PRODUCTS" subtitle="See your list of products." />
+      {data || !isLoading ? 
+        <Box>
+
+        </Box>:<></>
+      }
     </Box>
   );
 };
