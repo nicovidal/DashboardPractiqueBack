cf9f9c3e5b4d5b38ceaebcb05bc8ad2d18047fff
nicovidal <nico.vidal.m@gmail.com>
2023-12-26 21:43:58 -0300

    endpoint store

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import { Dashboard } from "./scenes/dashboard/index.jsx";
 import {Layout} from "./scenes/layout/index.jsx"
 import {Products} from "./scenes/products/index.jsx"
 import {Customers} from "./scenes/customers/index.jsx"
+import {Transactions} from "./scenes/transactions/index.jsx"
 
 export const App = () => {
   const mode =useSelector((state)=>state.global.mode);
@@ -24,6 +25,7 @@ export const App = () => {
             <Route path="/dashboard"element={<Dashboard />}/>  
             <Route path="/products"element={<Products />}/>  
             <Route path="/customers"element={<Customers />}/>  
+            <Route path="/transactions"element={<Transactions />}/>  
           </Route>
         </Routes>
         </ThemeProvider>
--- client/src/state/api.js
+++ client/src/state/api.js
@@ -3,29 +3,34 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_BASE_URL }),
   reducerPath: "adminApi",
-  tagTypes: [
-    "User","Products","Customers"
-  ],
+  tagTypes: ["User", "Products", "Customers", "Transactions"],
   endpoints: (build) => ({
     getUser: build.query({
       query: (id) => `general/user/${id}`,
       providesTags: ["User"],
     }),
-    getProducts:build.query({
-      query:()=>"client/products",
-      providesTags:["Products"],
+    getProducts: build.query({
+      query: () => "client/products",
+      providesTags: ["Products"],
     }),
-    getCustomers:build.query({
-      query:()=>"client/customers",
-      providesTags:["Customers"],
-    })
+    getCustomers: build.query({
+      query: () => "client/customers",
+      providesTags: ["Customers"],
+    }),
+  }),
+  getTransactions: build.query({
+    query: ({ page, pageSize, sort, search }) => ({
+      url: "client/transactions",
+      method: "GET",
+      params: { page, pageSize, sort, search },
+    }),
+    providesTags: ["Transactions"],
   }),
 });
 
 export const {
-    useGetUserQuery,
-    useGetProductsQuery,
-    useGetCustomersQuery,
-
-}=api;
-
+  useGetUserQuery,
+  useGetProductsQuery,
+  useGetCustomersQuery,
+  useGetTransactionsQuery,
+} = api;
