1712a69ecb7552eb26243d5ca140daf215df07cc
nicovidal <nico.vidal.m@gmail.com>
2023-12-26 21:39:02 -0300

    transaction end points

--- server/index.js
+++ server/index.js
@@ -14,8 +14,9 @@ import salesRoutes from "./routes/sales.js";
 import User from "./models/User.js";
 import Product from "./models/Products.js";
 import ProductStat from "./models/ProductStat.js";
+import Transaction from "./models/Transaction.js";
 
-import { dataUser, dataProduct, dataProductStat } from "./data/index.js";
+import { dataUser, dataProduct, dataProductStat ,dataTransaction} from "./data/index.js";
 
 /*config */
 dotenv.config();
@@ -45,5 +46,6 @@ mongoose
  /*         User.insertMany(dataUser) 
      Product.insertMany(dataProduct);
     ProductStat.insertMany(dataProductStat); */
+    /* Transaction.insertMany(dataTransaction); */
   })
   .catch((error) => console.log(`${error}did not connect`));
